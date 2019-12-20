b944c0bff40a859f9755bc7b1005f9df0f0d7253
CoffeeHaus <neumann6994@gmail.com>
2019-12-20 09:39:52 -0600

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,16 +1,38 @@
-with open(text_file, "wb") as fw:
-    print("Receiving..")
-    while True:
-        print('receiving')
-        data = conn.recv(32)
-        if data == b'BEGIN':
-            continue
-        elif data == b'ENDED':
-            print('Breaking from file write')
-            break
-        else:
-            print('Received: ', data.decode('utf-8'))
-            fw.write(data)
-            print('Wrote to file', data.decode('utf-8'))
-    fw.close()
-    print("Received..")
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+db_connect = create_engine('sqlite:///chinook.db')
+app = Flask(__name__)
+api = Api(app)
+port = 4545
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from employees") # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+        
+
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+
+if __name__ == '__main__':
+     app.run(port=port)
